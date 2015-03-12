2010e85fa73b637c899f96fee6b6d191bb5df996
shpytalenko <shpytalenko@gmail.com>
2015-03-12 08:19:51 +0600

    formulas bug update

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,9 +2,19 @@ class Day < ActiveRecord::Base
   has_many :reports
   before_create :set_total
   def set_total
-   self.total_tips_per_hour = (self.total_tips.to_i/self.total_hours.to_i).to_s
+   self.total_tips_per_hour = (self.total_tips.to_d/self.total_time.to_d).to_s
   rescue
     nil
   end
 
+  def total_time
+    self.reports.map {|x| x.total_hours.to_d}.inject(0, :+).to_s
+  end
+  def total_tips_hour
+    (self.total_tips.to_d/self.total_time.to_d).to_s
+  end
+
+  def total_compl
+    self.reports.map {|x| x.salary_complition.to_d rescue 0}.inject(0, :+).to_s
+  end
 end
