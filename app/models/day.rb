d52172f62760f33b7f61714b6027897dab70625b
shpytalenko <shpytalenko@gmail.com>
2015-03-12 07:42:30 +0600

    formula updates

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,3 +1,10 @@
 class Day < ActiveRecord::Base
   has_many :reports
+  before_create :set_total
+  def set_total
+   self.total_tips_per_hour = (self.total_tips.to_i/self.total_hours.to_i).to_s
+  rescue
+    nil
+  end
+
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,8 +1,15 @@
 class Report < ActiveRecord::Base
   belongs_to :worker
-  #before_create :set_total
+  before_create :set_total
   belongs_to :day
   def set_total
-    self.total_after_complition = (self.tips.to_i + self.salary_complition).to_s 
+    self.total_after_complition = (self.total_hours.to_d*35).to_s 
+    self.tips = (self.day.total_tips_per_hour.to_d*self.total_hours.to_d).to_s
+    self.salary_complition = (self.total_after_complition.to_d - self.tips.to_d).to_s
   end
+  
+  def total_hours
+    (clock_out - clock_in)/3600 rescue '0'
+  end
+
 end
